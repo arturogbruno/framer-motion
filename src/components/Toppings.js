404f6cd63c3f35f1fb85f7aed8f4d53a9b7254cd
Arturo G. Bruno <arturogbruno@gmail.com>
2020-07-17 17:42:39 +0200

    Add route's animations

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,6 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw"
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5
+    }
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" }
+  }
+}
+
 const buttonVariants = {
   hover: {
     scale: 1.1,
@@ -18,7 +37,12 @@ const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
   return (
-    <div className="toppings container">
+    <motion.div className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       
       <h3>Step 2: Choose Toppings</h3>
       <ul>
@@ -43,7 +67,7 @@ const Toppings = ({ addTopping, pizza }) => {
         </motion.button>
       </Link>
 
-    </div>
+    </motion.div>
   )
 }
 

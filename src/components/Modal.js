6ced716d3e228b9e5889716eab203d401b1a5127
Arturo G. Bruno <arturogbruno@gmail.com>
2020-07-17 18:03:00 +0200

    Add modal component and animation

---
+++ src/components/Modal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const brackdropVariants = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 1
+    }    
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+    return (
+        <AnimatePresence exitBeforeEnter>
+            {showModal && (
+                <motion.div className="backdrop"
+                    variants={brackdropVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+
+                </motion.div>
+            )}
+        </AnimatePresence>
+    )
+};
+
+export default Modal;
